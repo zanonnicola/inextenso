bc42a73afd74359c794fd8d39c92564ed536a2ab
zanonnicola <nicola.zanon@springernature.com>
2019-06-21 10:45:46 +0200

    Add simple rating system

--- functions/rate/index.ts
+++ functions/rate/index.ts
@@ -1,4 +1,6 @@
-import { NowRequest, NowResponse } from '@now/node'
+import { NowRequest, NowResponse } from '@now/node';
+import { RedisClient } from 'redis';
+import { AnyRecord } from 'dns';
 const redis = require("redis");
 const { promisify } = require("util");
 
@@ -8,9 +10,19 @@ const REDIS_URL = isDev ? "127.0.0.1" : process.env.REDIS_URL;
 const REDIS_PSW = process.env.REDIS_PSW;
 const REDIS_PORT = isDev ? 6379 : 14506;
 
-const client = redis.createClient(REDIS_PORT, REDIS_URL, {
+type Rates = '1' | '2' | '3' | '4' | '5'
+interface IPayload {
+  ip?: string;
+  feedback: {
+    [k in Rates]?: number;
+  }
+}
+
+const client: RedisClient = redis.createClient(REDIS_PORT, REDIS_URL, {
   no_ready_check: isDev ? false : true
 });
+
+const getAsync = promisify(client.get).bind(client);
 const setAsync = promisify(client.set).bind(client);
 
 export default async (req: NowRequest, res: NowResponse) => {
@@ -26,11 +38,29 @@ export default async (req: NowRequest, res: NowResponse) => {
     console.log(`Error: ${err}`);
     res.status(500).send(`Error: ${err}`);
   });
-  const { id, rate } = req.query;
+  const { id, rate } = <{ id: string, rate: string }>req.query;
   if (!id) {
     return res.status(400).send('id is missing from query parameters')
   }
-  const response = await setAsync(id, rate);
+  let response: string;
+  const currentObj = await getAsync(id);
+  console.log(currentObj);
+  if (currentObj !== null) {
+    const payload = JSON.parse(currentObj);
+    if (payload.feedback.hasOwnProperty(rate)) {
+      payload.feedback[rate]++;
+    } else {
+      payload.feedback[rate] = 0;
+    }
+    response = await setAsync(id, JSON.stringify(payload));
+  } else {
+    const payload: IPayload = {
+      feedback: {
+        [rate]: 0
+      }
+    }
+    response = await setAsync(id, JSON.stringify(payload));
+  }
   return res.status(200).send(
     JSON.stringify({
       data: {
